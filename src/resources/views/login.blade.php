71a5d9717995dfb9fc6d3f0b69ac1111ee9c7372
Piotr Grochowski <piogrek@gmail.com>
2023-05-02 14:18:34 +0100

    fixing composer deps

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -9,7 +9,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-            @routes
             @viteReactRefresh
             @vite('resources/css/app.css')
 
